91667eefe9fdf865a1cda0a08770f8e91c4c0a7f
viveklb <69708672+viveklb@users.noreply.github.com>
2026-06-15 04:53:48 +0000

    Implement handler props for color selection and box styling

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
-  const { background } = config;
+  const { config, selectNextBackground } = props;
+  const { background, classname, label } = config;
+
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button
+      className={classname}
+      onClick={() => selectNextBackground({ background })}
+    >
+      {label}
     </button>
-  )
-}
+  );
+};
+
 export default ColourSelector;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, { useState } from 'react';
 
+const Selection = ({ applyColor }) => {
+  const [selectionStyle, updateSelectionStyle] = useState({
+    background: ''
+  });
+
+  return (
+    <div
+      className="fix-box"
+      style={selectionStyle}
+      onClick={() => applyColor(updateSelectionStyle)}
+    />
+  );
+};
+
+export default Selection;
